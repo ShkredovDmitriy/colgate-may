f2ff3206e7e60d437de898dc622343e93fe4c16b
Dima <shkredovdmitriy@gmail.com>
2020-05-03 00:09:49 +0300

    Layout Desktop

--- dev/app/main.js
+++ dev/app/main.js
@@ -1,3 +1,26 @@
 import '../common/scss/main.scss';
 
 require('./polyfills/polyfills');
+
+if (document.querySelector('.main-header__sandwich')) {
+  //
+  $('.main-header__sandwich').click(() => {
+    $('.main-header__sandwich').toggleClass('active');
+    $('.nav-menu').toggleClass('active');
+    $('body').toggleClass('overflov-hidden');
+  });
+
+  //
+  $('.nav-menu__link').click(() => {
+    $('.main-header__sandwich').removeClass('active');
+    $('.nav-menu').removeClass('active');
+    $('body').removeClass('overflov-hidden');
+  });
+
+  //
+  $('.nav-menu__social-link').click(() => {
+    $('.main-header__sandwich').removeClass('active');
+    $('.nav-menu').removeClass('active');
+    $('body').removeClass('overflov-hidden');
+  });
+}
